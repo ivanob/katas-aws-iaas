f151b26c9355ba875681951e549c5e9d962098b9
Ivan Obeso <ivan.obeso1@gmail.com>
2023-04-01 20:27:36 +0200

    added exercise of simple website on s3

---
+++ packages/static-s3-cloudfront/README.md
@@ -0,0 +1,5 @@
+
+Bibliography:
+
+- https://www.youtube.com/watch?v=mls8tiiI3uc
+- https://www.youtube.com/watch?v=06VgLTqNvU8
---
+++ packages/static-s3-cloudfront/infra/s3.tf
@@ -0,0 +1,52 @@
+resource "aws_s3_bucket" "test_static_web" {
+  bucket = "www.myteststaticweb.io"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "set_acl" {
+  bucket = aws_s3_bucket.test_static_web.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "enable_public_access" {
+  bucket = aws_s3_bucket.test_static_web.id
+
+  # Disable Block all public access
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+// This declaration uploads the files from the build folder to the s3 bucket
+resource "aws_s3_bucket_object" "react_app_files" {
+  for_each = fileset("${path.module}/../src/demo-app/build", "**/*")
+
+  bucket = aws_s3_bucket.test_static_web.id
+  key = each.value
+  source = "${path.module}/../src/demo-app/build/${each.value}"
+}
+
+// This is the policy to give access from outside to the s3 objects
+resource "aws_s3_bucket_policy" "public_access_policy" {
+  bucket = aws_s3_bucket.test_static_web.id
+
+  # Set up bucket policy to allow public read access
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = ["s3:*"]
+        Resource = ["${aws_s3_bucket.test_static_web.arn}/*"]
+      }
+    ]
+  })
+}
